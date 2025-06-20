9e094409aff3859cf4f95a7d0e0c7825eaea8e6c
Lolslark <danikwow1903@gmail.com>
2025-06-20 14:53:38 +0300

    Add MyArrayList

---
+++ step2/MyArrayList.java
@@ -0,0 +1,69 @@
+package Aston.step2;
+
+import java.util.Arrays;
+
+public class MyArrayList<E>{
+    private Object[] array;
+    private int size;
+    private static final int DEFAULT_CAPACITY = 10;
+
+    public MyArrayList(){
+        this.array = new Object[DEFAULT_CAPACITY];
+        this.size = 0;
+    }
+
+    public int size() { // размер массива
+        return size;
+    }
+
+    public void add (int element){  // добавление в массив
+        array[size++] = element;
+    }
+
+    public Object get (int index){  //взять из массива
+        return array[index];
+    }
+
+    public Object remove (int index){ //удалить из массива
+        for (int i = index; i < array.length - 1; i++){
+            array[i] = array[i+1];
+        }
+        size --;
+        return array;
+    }
+
+    public void addAll (MyArrayList <E> list){  //добавить всё
+        for (int i = 0; i < list.size(); i++){
+            add(i);
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "MyArrayList{" +
+                "array=" + Arrays.toString(array) +
+                ", size=" + size +
+                '}';
+    }
+
+    public static void main(String[] args) {
+        MyArrayList<Integer> list = new MyArrayList<>();
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        System.out.println(list);
+
+        System.out.println(list.get(0));
+
+        list.remove(1);
+
+        System.out.println(list);
+
+        MyArrayList<Integer> list1 = new MyArrayList<>();
+
+        list1.addAll(list);
+
+        System.out.println(list1);
+
+    }
+}
