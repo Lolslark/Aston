f6316952d7c5a5de25fa44bafb41b8af040aae29
achadaev <andrew.chadaev@gmail.com>
2025-06-26 20:59:48 +0500

    Refactor step3 solution

--- step3/FileUtils.java
+++ step3/FileUtils.java
@@ -1,9 +1,11 @@
 package Aston.step3;
 
-import java.io.*;
 import java.util.ArrayList;
 import java.io.FileWriter;
+import java.io.FileReader;
+import java.io.BufferedReader;
 import java.io.BufferedWriter;
+import java.io.IOException;
 import java.util.List;
 
 public class FileUtils {
@@ -14,10 +16,10 @@ public class FileUtils {
         try {
             fileRead(fileInputPath,list);
             if (!list.isEmpty()) {
-                print(list);
+                System.out.println(list);
                 fileWrite(fileOutputPath, list);
-            }
-            else System.out.print("файл не содержит данных.");
+            } else
+                System.out.print("Файл не содержит данных.");
         }
         catch (MyException e) {
             System.err.println("Ошибка при обработке файла: " + e.getMessage());
@@ -25,7 +27,7 @@ public class FileUtils {
         }
     }
 
-    public static void fileRead (String inputPath, List<String> list) throws MyException {
+    public static void fileRead(String inputPath, List<String> list) throws MyException {
         try (BufferedReader reader = new BufferedReader(new FileReader(inputPath))) {
             System.out.print("Содержимое файла: ");
             String line;
@@ -37,18 +39,14 @@ public class FileUtils {
         }
     }
 
-    public static void fileWrite(String outPath, List<String> list) throws MyException {
-        try (BufferedWriter writer = new BufferedWriter(new FileWriter(outPath))) {
+    public static void fileWrite(String outputPath, List<String> list) throws MyException {
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(outputPath))) {
             for (String s : list) {
                 writer.write(String.valueOf(s) + '\n');
             }
             System.out.println("Данные успешно записаны в файл.");
         } catch (IOException e) {
-            throw new MyException("Ошибка при записи в файл: " + outPath, e);
+            throw new MyException("Ошибка при записи в файл: " + outputPath, e);
         }
-    }
-
-    public static void print(List<String> list) {
-        System.out.println(list);
     }
 }
